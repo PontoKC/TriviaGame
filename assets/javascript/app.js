9d281b5aa16196a0540296af55319cbf596bd0c8
unknown <daniel.pontarelli@gmail.com>
2017-07-28 14:07:49 -0500

    Initial file structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+//User will press the button on index.html to start.
+  //Button will change the contents of the Jumbotron to the questions with form input.  
+  //user will only be able to select one answer per question and will only have 1 minute to answer as many as possible.
+  //Once the game ends, the jumbotron contents will change again to show the results of the quiz.
+  //User will reload page to try again -- could add a button that would let them restart.
+
+  var right = 0;
+  var wrong = 0;
+
+
+//Objects for each quesiton 
+questions: {
+	
+
+
+}
+
+
+//fuction to run when page loads that 
